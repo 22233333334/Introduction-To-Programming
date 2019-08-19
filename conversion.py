f7c7f80997b321c156c7e6e53553cc7e4c59785d
JeffGeyer <jeffreygeyer@gmail.com>
2019-08-19 22:10:06 +0200

    Conversion (Python)
    Examples of casting using Python

---
+++ conversion.py
@@ -0,0 +1,23 @@
+# Declare the variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Convert float to int
+num1 = int(num1)
+
+# Convert int to float
+num2 = float(num2)
+
+# Convert int to string
+num3 = str(num3)
+
+# Convert string to int
+string1 = int(string1)
+
+# Print out all the variables
+print(num1)
+print(num2)
+print(num3)
+print(string1)
