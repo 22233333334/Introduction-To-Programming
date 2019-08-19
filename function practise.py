520f3274f2da42ec3fe2ca7ddf2f6b630af01543
JeffGeyer <jeffreygeyer@gmail.com>
2019-08-19 23:13:58 +0200

    Function Practise (Python)
    Simple function to add three numbers

---
+++ function practise.py
@@ -0,0 +1,10 @@
+#   Define function to add three numbers
+def addthree(num1,num2,num3):
+    y = num1 + num2 + num3
+    return y
+
+#   Create variable to store sum using function
+sumFunc = (addthree(52,25,1403))
+
+#   Print out sum
+print(str(sumFunc))
