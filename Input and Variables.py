3e83af5aad54f5db1e28f72cede5f50b97d24204
JeffGeyer <jeffreygeyer@gmail.com>
2019-08-19 22:03:20 +0200

    Input and Variables (Python)
    The code asks for user input and prints out the information using f strings.

---
+++ Input and Variables.py
@@ -0,0 +1,7 @@
+# Ask user for their name, age and address
+userName = input("Please enter your name: ")
+userAge = input("Please enter your age: ")
+userAddress = input("Please enter your address: ")
+
+# Print out the information
+print(f"The user's name is: {userName}. He/She is {userAge} years old. Residence is: {userAddress}")
